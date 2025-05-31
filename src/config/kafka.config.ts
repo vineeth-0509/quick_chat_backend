5fada70137bf79ca4c74bbba170ecfeb36af258e
Vineeth-0509 <vineeththungani@gmail.com>
2025-05-31 10:16:19 +0530

    kafka-changes

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -1,10 +1,15 @@
-import { Kafka } from "kafkajs";
-
+import {Kafka} from "kafkajs"
 export const kafka = new Kafka({
-  brokers: [process.env.KAFKA_BROKER!],
-  clientId: "quick-chat",
-});
+    clientId:"Quick_chat",
+    brokers:[process.env.KAFKA_BOOTSTRAP_SERVER_URL],
+    ssl:true,
+    sasl:{
+        mechanism:"plain",
+        username:process.env.KAFKA_API_KEY,
+        password:process.env.KAFKA_API_SECRET
+    }
 
+});
 export const producer = kafka.producer();
 export const consumer = kafka.consumer({ groupId: "chats" });
 
